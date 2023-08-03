72c6278e5883d2221ce64ef853879bd85c8bf36c
JisuSeong <sjangela0615@gmail.com>
2023-08-03 17:15:48 +0900

    영화, 장소 객체생성, 영화, 장소 DAO 생성

---
+++ src/object/User.java
@@ -0,0 +1,64 @@
+package object;
+
+import java.sql.Blob;
+
+public class User {
+	private int userno;
+	private String id;
+	private String password;
+	private String nickname;
+	private Blob profile;
+
+	public User(int userno, String id, String password) {
+		super();
+		this.userno = userno;
+		this.id = id;
+		this.password = password;
+	}
+
+	public User(int userno, String id, String password, String nickname, Blob profile) {
+		this.userno = userno;
+		this.id = id;
+		this.password = password;
+		this.nickname = nickname;
+		this.profile = profile;
+	}
+
+	public int getUserno() {
+		return userno;
+	}
+
+	public void setUserno(int userno) {
+		this.userno = userno;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public Blob getProfile() {
+		return profile;
+	}
+
+	public void setProfile(Blob profile) {
+		this.profile = profile;
+	}
+}

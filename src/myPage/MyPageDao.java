3ac4575007713db8014efa1175ede738df3f09c9
Psh230412 <sanghyeon0402@gmail.com>
2023-08-07 16:29:12 +0900

    메이븐 설정

--- src/myPage/MyPageDao.java
+++ src/myPage/MyPageDao.java
@@ -1,6 +1,5 @@
 package myPage;
 
-import java.sql.Blob;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -9,62 +8,60 @@ import java.sql.SQLException;
 import dbutil.DBUtil;
 
 public class MyPageDao {
-	
-	public String getNickname(String id) {
-		Connection conn = null;
-		PreparedStatement stmt = null;
-		ResultSet rs = null;
-		
-		String nickname = "";
-		try {
-			conn = DBUtil.getConnection();
-			stmt = conn.prepareStatement("SELECT * FROM user WHERE id = ?");
-			stmt.setString(1, id);
-			rs = stmt.executeQuery();
 
-			if (rs.next()) {
-				nickname = rs.getString("nickname");
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			DBUtil.close(rs);
-			DBUtil.close(stmt);
-			DBUtil.close(conn);
-		}
-		return nickname;
-	}
-	
-	public List<Path> getMyPath(String id) {
-		Connection conn = null;
-		PreparedStatement stmt = null;
-		ResultSet rs = null;
-		
-		try {
-			conn = DBUtil.getConnection();
-			stmt = conn.prepareStatement("SELECT location1\r\n" + 
-					"					FROM path\r\n" + 
-					"					WHERE user_choice_no = (SELECT user_choice_no\r\n" + 
-					"					FROM user_choice\r\n" + 
-					"					WHERE userno = 1);");
-			stmt.setString(1, id);
-			rs = stmt.executeQuery();
+    public String getNickname(String id) {
+	Connection conn = null;
+	PreparedStatement stmt = null;
+	ResultSet rs = null;
 
-			if (rs.next()) {
-				String location1 = rs.getString("location1");
-				String location2 = rs.getString("location2");
-				String location3 = rs.getString("location3");
-				String location4 = rs.getString("location4");
-				Blob pathMapImage = rs.getBlob("pathMapImage");
-				
-				
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			DBUtil.close(rs);
-			DBUtil.close(stmt);
-			DBUtil.close(conn);
-		}
+	String nickname = "";
+	try {
+	    conn = DBUtil.getConnection();
+	    stmt = conn.prepareStatement("SELECT * FROM user WHERE id = ?");
+	    stmt.setString(1, id);
+	    rs = stmt.executeQuery();
+
+	    if (rs.next()) {
+		nickname = rs.getString("nickname");
+	    }
+	} catch (SQLException e) {
+	    e.printStackTrace();
+	} finally {
+	    DBUtil.close(rs);
+	    DBUtil.close(stmt);
+	    DBUtil.close(conn);
 	}
+	return nickname;
+    }
+
+//    public List<Path> getMyPath(String id) {
+//	Connection conn = null;
+//	PreparedStatement stmt = null;
+//	ResultSet rs = null;
+//
+//	try {
+//	    conn = DBUtil.getConnection();
+//	    stmt = conn.prepareStatement("SELECT location1\r\n" + "					FROM path\r\n"
+//		    + "					WHERE user_choice_no = (SELECT user_choice_no\r\n"
+//		    + "					FROM user_choice\r\n"
+//		    + "					WHERE userno = 1);");
+//	    stmt.setString(1, id);
+//	    rs = stmt.executeQuery();
+//
+//	    if (rs.next()) {
+//		String location1 = rs.getString("location1");
+//		String location2 = rs.getString("location2");
+//		String location3 = rs.getString("location3");
+//		String location4 = rs.getString("location4");
+//		Blob pathMapImage = rs.getBlob("pathMapImage");
+//
+//	    }
+//	} catch (SQLException e) {
+//	    e.printStackTrace();
+//	} finally {
+//	    DBUtil.close(rs);
+//	    DBUtil.close(stmt);
+//	    DBUtil.close(conn);
+//	}
+//    }
 }

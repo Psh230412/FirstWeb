1eaad89ce790fd19ee577bc112d7a3a1e7c22a3f
LeeJungbin <dlwjdqls783@gmail.com>
2023-08-07 15:39:46 +0900

    마이페이지 수정중

--- src/myPage/MyPageDao.java
+++ src/myPage/MyPageDao.java
@@ -1,5 +1,6 @@
 package myPage;
 
+import java.sql.Blob;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -32,5 +33,38 @@ public class MyPageDao {
 			DBUtil.close(conn);
 		}
 		return nickname;
+	}
+	
+	public List<Path> getMyPath(String id) {
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		
+		try {
+			conn = DBUtil.getConnection();
+			stmt = conn.prepareStatement("SELECT location1\r\n" + 
+					"					FROM path\r\n" + 
+					"					WHERE user_choice_no = (SELECT user_choice_no\r\n" + 
+					"					FROM user_choice\r\n" + 
+					"					WHERE userno = 1);");
+			stmt.setString(1, id);
+			rs = stmt.executeQuery();
+
+			if (rs.next()) {
+				String location1 = rs.getString("location1");
+				String location2 = rs.getString("location2");
+				String location3 = rs.getString("location3");
+				String location4 = rs.getString("location4");
+				Blob pathMapImage = rs.getBlob("pathMapImage");
+				
+				
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			DBUtil.close(rs);
+			DBUtil.close(stmt);
+			DBUtil.close(conn);
+		}
 	}
 }

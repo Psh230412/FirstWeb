86bc08c2bbf06c5a6a62e62ad1804dd23574638b
JeongBin0462 <dlwjdqls783@gmail.com>
2023-08-08 14:50:34 +0900

    인코딩오류 수정

--- src/login/LoginServelt.java
+++ src/login/LoginServelt.java
@@ -15,82 +15,81 @@ import join.JoinDao;
 
 @WebServlet("/login")
 public class LoginServelt extends HttpServlet {
-   LoginDao dao = new LoginDao();
-   JoinDao joinDao = new JoinDao();
+	LoginDao dao = new LoginDao();
+	JoinDao joinDao = new JoinDao();
 
-   @Override
-   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-      req.getRequestDispatcher("/WEB-INF/loginPage/login.jsp").forward(req, resp);
-   }
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.getRequestDispatcher("/WEB-INF/loginPage/login.jsp").forward(req, resp);
+	}
 
-   @Override
-   protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-      String formType = req.getParameter("form_type");
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String formType = req.getParameter("form_type");
 
-      if (formType.equals("loginForm")) {
-         String id = req.getParameter("id");
-         String password = req.getParameter("password");
-         boolean remember = false;
+		if (formType.equals("loginForm")) {
+			String id = req.getParameter("id");
+			String password = req.getParameter("password");
+			boolean remember = false;
 
-         if (req.getParameter("remember") != null) {
-            remember = req.getParameter("remember").equals("check");
-         }
-         if (dao.checkId(id)) {
-            if (dao.checkPassword(id, password)) {
-               if (remember) {
-                  Cookie cookie = new Cookie("remember", id);
-                  resp.addCookie(cookie);
-               }
-               HttpSession session = req.getSession();
-               session.setAttribute("loggedUserId", id);
-            
-               // ´ÙÀ½ÆäÀÌÁö·Î ÀÌµ¿
-               resp.sendRedirect("main/index.html");
-            } else {
-               req.setAttribute("loginError", "¾ÆÀÌµğ ¶Ç´Â ºñ¹Ğ¹øÈ£¸¦ Æ²¸®¼Ì½À´Ï´Ù.");
-               req.getRequestDispatcher("/WEB-INF/loginPage/login.jsp").forward(req, resp);
-            }
-         } else {
-            req.setAttribute("loginError", "¾ÆÀÌµğ ¶Ç´Â ºñ¹Ğ¹øÈ£¸¦ Æ²¸®¼Ì½À´Ï´Ù.");
-            req.getRequestDispatcher("/WEB-INF/loginPage/login.jsp").forward(req, resp);
-         }
-      } else if (formType.equals("joinForm")) {
-         String joinNickname = req.getParameter("joinNickname");
-         String joinId = req.getParameter("joinId");
-         String joinPassword = req.getParameter("joinPassword");
-         String joinPasswordRe = req.getParameter("joinPasswordRe");
+			if (req.getParameter("remember") != null) {
+				remember = req.getParameter("remember").equals("check");
+			}
+			if (dao.checkId(id)) {
+				if (dao.checkPassword(id, password)) {
+					if (remember) {
+						Cookie cookie = new Cookie("remember", id);
+						resp.addCookie(cookie);
+					}
+					HttpSession session = req.getSession();
+					session.setAttribute("loggedUserId", id);
+					// ë‹¤ìŒí˜ì´ì§€ë¡œ ì´ë™
+					resp.sendRedirect("main/index.html");
+				} else {
+					req.setAttribute("loginError", "ì•„ì´ë”” ë˜ëŠ” ë¹„ë°€ë²ˆí˜¸ë¥¼ í‹€ë¦¬ì…¨ìŠµë‹ˆë‹¤.");
+					req.getRequestDispatcher("/WEB-INF/loginPage/login.jsp").forward(req, resp);
+				}
+			} else {
+				req.setAttribute("loginError", "ì•„ì´ë”” ë˜ëŠ” ë¹„ë°€ë²ˆí˜¸ë¥¼ í‹€ë¦¬ì…¨ìŠµë‹ˆë‹¤.");
+				req.getRequestDispatcher("/WEB-INF/loginPage/login.jsp").forward(req, resp);
+			}
+		} else if (formType.equals("joinForm")) {
+			String joinNickname = req.getParameter("joinNickname");
+			String joinId = req.getParameter("joinId");
+			String joinPassword = req.getParameter("joinPassword");
+			String joinPasswordRe = req.getParameter("joinPasswordRe");
 
-         boolean isJoin = true;
+			boolean isJoin = true;
 
-         if (!joinDao.duplicateNickname(joinNickname)) {
-            req.removeAttribute("joinNicknameError");
-         } else {
-            req.setAttribute("inputNickname", joinNickname);
-            req.setAttribute("joinNicknameError", "°°Àº ´Ğ³×ÀÓÀÌ Á¸ÀçÇÕ´Ï´Ù.");
-            isJoin = false;
-         }
+			if (!joinDao.duplicateNickname(joinNickname)) {
+				req.removeAttribute("joinNicknameError");
+			} else {
+				req.setAttribute("inputNickname", joinNickname);
+				req.setAttribute("joinNicknameError", "ê°™ì€ ë‹‰ë„¤ì„ì´ ì¡´ì¬í•©ë‹ˆë‹¤.");
+				isJoin = false;
+			}
 
-         if (!joinDao.duplicateId(joinId)) {
-            req.removeAttribute("joinIdError");
-         } else {
-            req.setAttribute("inputId", joinId);
-            req.setAttribute("joinIdError", "°°Àº ÀÌ¸§ÀÇ ¾ÆÀÌµğ°¡ Á¸ÀçÇÕ´Ï´Ù.");
-            isJoin = false;
-         }
+			if (!joinDao.duplicateId(joinId)) {
+				req.removeAttribute("joinIdError");
+			} else {
+				req.setAttribute("inputId", joinId);
+				req.setAttribute("joinIdError", "ê°™ì€ ì´ë¦„ì˜ ì•„ì´ë””ê°€ ì¡´ì¬í•©ë‹ˆë‹¤.");
+				isJoin = false;
+			}
 
-         if (joinPassword.equals(joinPasswordRe)) {
-            req.removeAttribute("joinPasswordError");
-         } else {
-            req.setAttribute("joinPassword", joinPassword);
-            req.setAttribute("joinPasswordRe", joinPasswordRe);
-            req.setAttribute("joinPasswordError", "ÀÔ·ÂÇÑ ºñ¹Ğ¹øÈ£°¡ ¼­·Î ´Ù¸¨´Ï´Ù.");
-            isJoin = false;
-         }
+			if (joinPassword.equals(joinPasswordRe)) {
+				req.removeAttribute("joinPasswordError");
+			} else {
+				req.setAttribute("joinPassword", joinPassword);
+				req.setAttribute("joinPasswordRe", joinPasswordRe);
+				req.setAttribute("joinPasswordError", "ì…ë ¥í•œ ë¹„ë°€ë²ˆí˜¸ê°€ ì„œë¡œ ë‹¤ë¦…ë‹ˆë‹¤.");
+				isJoin = false;
+			}
 
-         if (isJoin) {
-            dao.insertId(joinNickname, joinId, joinPassword);
-         }
-         req.getRequestDispatcher("/WEB-INF/loginPage/login.jsp").forward(req, resp);
-      }
-   }
+			if (isJoin) {
+				dao.insertId(joinNickname, joinId, joinPassword);
+			}
+			req.getRequestDispatcher("/WEB-INF/loginPage/login.jsp").forward(req, resp);
+		}
+	}
 }

dd744e566d9b0017862105a8e1f77ba5933a9d13
LeeKH2 <caniopy1@naver.com>
2023-08-11 16:36:39 +0900

    머지 완료

--- src/selectLocation/LocationServlet.java
+++ src/selectLocation/LocationServlet.java
@@ -10,7 +10,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.security.auth.message.callback.PrivateKeyCallback.Request;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -21,8 +20,8 @@ import javax.servlet.http.HttpSession;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import object.Location;
 import object.ImgLocationObject;
+import object.Location;
 
 @WebServlet("/flow")
 public class LocationServlet extends HttpServlet {
@@ -30,21 +29,22 @@ public class LocationServlet extends HttpServlet {
 
 	private void MoviesPoster(HttpServletRequest req, List<Integer> movieNumbers) {
 		List<String> encodedPosters = new ArrayList<>();
-		Map<Integer, String> postersMap = new HashMap<>();
+		Map<String, String> postersMap = new HashMap<>();
 
 		for (Integer movieNumber : movieNumbers) {
 			Blob posterBlob = movieDao.selectOneMovie(movieNumber);
+			String title = movieDao.selectOneTitle(movieNumber);
 			if (posterBlob != null) {
 				try {
 					byte[] posterBytes = posterBlob.getBytes(1, (int) posterBlob.length());
 					String encodedPoster = Base64.getEncoder().encodeToString(posterBytes);
-					postersMap.put(movieNumber, encodedPoster);
+					postersMap.put(title, encodedPoster);
 				} catch (SQLException e) {
 					e.printStackTrace();
 				}
 			}
 		}
-
+		req.setAttribute("movieNumber", movieNumbers);
 		req.setAttribute("postersMap", postersMap);
 	}
 
@@ -55,8 +55,10 @@ public class LocationServlet extends HttpServlet {
 		for (Integer movieNumber : movieNumbers) {
 			List<ImgLocationObject> locationsList = new ArrayList<>();
 			List<Location> locations = movieDao.selectLocationList(movieNumber);
+			String title = null;
 
 			for (Location location : locations) {
+				title = movieDao.selectOneTitle(movieNumber);
 				List<String> singleImageData = new ArrayList<>();
 				List<String> singleAddressData = new ArrayList<>();
 				List<Integer> location_no = new ArrayList<>();
@@ -72,8 +74,8 @@ public class LocationServlet extends HttpServlet {
 
 				locationsList.add(new ImgLocationObject(singleImageData, singleAddressData, location_no));
 			}
-			System.out.println("Movie Number: " + movieNumber + ", Locations: " + locationsList);
-			movieLocationsMap.put(String.valueOf(movieNumber), locationsList);
+//			System.out.println("Movie Number: " + movieNumber + ", Locations: " + locationsList);
+			movieLocationsMap.put(title, locationsList);
 		}
 
 		req.setAttribute("movieLocations", movieLocationsMap);
@@ -82,7 +84,7 @@ public class LocationServlet extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String movieNumbersJson = req.getParameter("movieNumbers");
-		
+
 		if (movieNumbersJson != null) {
 			HttpSession session = req.getSession();
 			session.setAttribute("movieNumbers", movieNumbersJson);
@@ -90,14 +92,14 @@ public class LocationServlet extends HttpServlet {
 			HttpSession session = req.getSession();
 			movieNumbersJson = (String) session.getAttribute("movieNumbers");
 		}
-		
+
 		HttpSession session = req.getSession();
 		String nickname = (String) session.getAttribute("loggedUserNickname");
 		String profileImg = (String) session.getAttribute("loggedUserProfileImg");
 
 		req.setAttribute("nickname", nickname);
 		req.setAttribute("porfileImg", profileImg);
-		
+
 		ObjectMapper mapper = new ObjectMapper();
 
 		try {
